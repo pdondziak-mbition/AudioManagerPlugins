2968b6817d0787d42801479f36da2f970946cd06
dondzip <pawel_jan.dondziak@mercedes-benz.com>
2023-11-03 22:07:02 +0100

    Registry working

--- PluginCommandInterfacePipewire/src/CAmCommandReceiverPipewire.cpp
+++ PluginCommandInterfacePipewire/src/CAmCommandReceiverPipewire.cpp
@@ -11,12 +11,44 @@ DLT_IMPORT_CONTEXT(commandPipewire)
 namespace am
 {
 
+void registry_event_global(void *data, uint32_t id,
+                uint32_t permissions, const char *type, uint32_t version,
+                const struct spa_dict *props)
+{
+    log(&commandPipewire, DLT_LOG_INFO, "object: id:", id, "type:", type, "/", version);
+}
+
+const struct pw_registry_events registry_events = {
+        PW_VERSION_REGISTRY_EVENTS,
+        .global = registry_event_global
+};
+ 
+
+
 // The function we want to execute on the new thread.
 void pwMainThread(pw_main_loop ** pwMainLoop)
 {
     log(&commandPipewire, DLT_LOG_INFO, "Inside pwMainThread");
 
     *pwMainLoop = pw_main_loop_new(NULL);
+
+    pw_context *context = pw_context_new(pw_main_loop_get_loop(*pwMainLoop), nullptr, 0);
+
+    pw_core * core = pw_context_connect(context, nullptr, 0);
+
+    if (core == nullptr)
+    {
+        log(&commandPipewire, DLT_LOG_ERROR, "Error connecting pipewire context!");
+        return;
+    }
+
+    spa_hook registry_listener;
+
+    pw_registry * registry = pw_core_get_registry(core, PW_VERSION_REGISTRY, 0);
+
+    spa_zero(registry_listener);
+
+    pw_registry_add_listener(registry, &registry_listener, &registry_events, nullptr);
 
     log(&commandPipewire, DLT_LOG_INFO, "entering loop");
     pw_main_loop_run(*pwMainLoop);
